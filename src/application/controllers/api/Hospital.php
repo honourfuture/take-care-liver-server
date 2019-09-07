60747ded640a3c4e7648ba4e3a496c9b616d7f3d
honourfuture@163.com <honourfuture@163.com>
2019-09-08 00:08:30 +0800

    'debug'

---
+++ src/application/controllers/api/Hospital.php
@@ -0,0 +1,89 @@
+<?php
+defined('BASEPATH') or exit ('No direct script access allowed');
+
+use Restserver\Libraries\REST_Controller;
+
+class Hospital extends REST_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Hospital_model');
+    }
+
+    private function json($data, $code = 200, $message = '获取数据成功!')
+    {
+        $res['status'] = $code;
+        $res['data'] = $data;
+        $res['msg'] = $message;
+        $this->response($res);
+    }
+
+    /**
+     * @SWG\Get(path="/hospital/list",
+     *   tags={"Hospital"},
+     *   summary="医院列表",
+     *   description="医院列表",
+     *   operationId="hospitalList",
+     *   produces={"application/json"},
+     *  @SWG\Parameter(
+     *     in="query",
+     *     name="cur_page",
+     *     description="当前页",
+     *     required=true,
+     *     type="integer"
+     *   ),
+     *  @SWG\Parameter(
+     *     in="query",
+     *     name="per_page",
+     *     description="每页数量 [默认10条]",
+     *     required=false,
+     *     type="integer"
+     *   ),
+     *   @SWG\Response(response="200", description="成功")
+     * )
+     */
+    public function list_get()
+    {
+        $data = $this->Hospital_model->getAllByCid($this->per_page, $this->offset);
+        if ($data) {
+            $this->json($data);
+        } else {
+            $this->json([], 0, $message = '没有数据');
+        }
+    }
+
+    /**
+     * @SWG\Get(path="/hospital/find",
+     *   tags={"Hospital"},
+     *   summary="医院详情",
+     *   description="医院详情",
+     *   operationId="hospitalFind",
+     *   produces={"application/json"},
+     *  @SWG\Parameter(
+     *     in="query",
+     *     name="id",
+     *     description="医院id",
+     *     required=false,
+     *     type="string"
+     *   ),
+     *   @SWG\Response(response="200", description="成功")
+     * )
+     */
+    public function find_get()
+    {
+        $id = $this->input->get('id');
+        $data = $this->Hospital_model->find($id);
+
+        if ($data) {
+            $this->json($data);
+        } else {
+            $this->json([], 0, $message = '没有数据');
+        }
+    }
+
+
+}
+
+?>
---
+++ src/application/models/Hospital_model.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ *  Admin Model
+ *
+ **/
+class Hospital_model extends CI_Model
+{
+    private $table = 'hospitals';
+
+    public function getAllByCid( $page, $offset)
+    {
+        $this->db->select('id,name,telphone,position,pic,detail');
+
+        $this->db->from($this->table);
+        $this->db->order_by("create_time", "DESC");
+        $this->db->limit($page, $offset);
+        $query = $this->db->get();
+
+        return $query->result();
+    }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    function create($data)
+    {
+        $this->db->insert($this->table, $data);
+
+        return $this->db->insert_id();
+    }
+
+    //总数
+
+    public function getCount($keyword = '')
+    {
+        $this->db->select('id');
+
+        if (!empty($keyword)) {
+            $this->db->like('name', $keyword, 'both');
+        }
+
+        $this->db->from($this->table);
+        $total = $this->db->count_all_results();
+
+        return $total;
+    }
+
+    /*
+    * 查找
+    */
+    function getAll($num = 30, $offset = 0)
+    {
+        $this->db->select('*');
+
+        $this->db->from($this->table);
+        $this->db->order_by('id', 'desc');
+        $this->db->limit($num, $offset);
+        $query = $this->db->get();
+
+        return $query->result();
+    }
+
+    /*
+    * 更新
+    */
+
+    function update($id, $data)
+    {
+
+        $this->db->where('id', $id);
+        $this->db->update($this->table, $data);
+    }
+
+    /*
+    * 删除
+    */
+
+    function delete($id)
+    {
+        return $this->db->delete($this->table, array('id' => $id));
+    }
+
+    /*
+    * 查询
+    */
+    function find($id)
+    {
+        $this->db->where('id', $id);
+        $query = $this->db->get($this->table);
+
+        return $query->row();
+
+    }
+}
