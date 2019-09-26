79ebe221b294a48c28117cb9e5242800833da48f
honourfuture <honourfuture@163.com>
2019-09-26 15:01:29 +0800

    debug

--- src/application/controllers/api/RetailStore.php
+++ src/application/controllers/api/RetailStore.php
@@ -70,6 +70,7 @@ class RetailStore extends REST_Controller
 
         $where = [
             'parent_id' => $this->user_id,
+            'is_vip' => 1
         ];
 
         $total = $this->User_model->getAllPageTotal($where);
